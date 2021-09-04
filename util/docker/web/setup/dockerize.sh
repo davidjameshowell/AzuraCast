9742debd52859e8ab503b2ca9c63561ddf5baceb
David Howell <david@davidjameshowell.com>
2021-09-04 06:09:05 +0000

    Update Dockerize for arm64

--- util/docker/web/setup/dockerize.sh
+++ util/docker/web/setup/dockerize.sh
@@ -3,7 +3,17 @@ set -e
 source /bd_build/buildconfig
 set -x
 
-DOCKERIZE_VERSION=v0.6.1
-wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
-tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
-rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
+ARCH=$(uname -r)
+SUB="aarch64"
+
+DOCKERIZE_ARCH="amd64"
+DOCKERIZE_VERSION="v0.6.1"
+
+if grep -q "$SUB" <<< "$ARCH"; then
+  echo "ARM detected, set arch version"
+  DOCKERIZE_ARCH="armhf"
+fi
+
+wget https://github.com/jwilder/dockerize/releases/download/${DOCKERIZE_VERSION}/dockerize-linux-${DOCKERIZE_ARCH}-${DOCKERIZE_VERSION}.tar.gz
+tar -C /usr/local/bin -xzvf dockerize-linux-${DOCKERIZE_ARCH}-${DOCKERIZE_VERSION}.tar.gz
+rm dockerize-linux-${DOCKERIZE_ARCH}-${DOCKERIZE_VERSION}.tar.gz
