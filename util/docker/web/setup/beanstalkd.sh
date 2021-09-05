493651a57241abe8d0a0392ceec9f54955f1f30d
David Howell <david@davidjameshowell.com>
2021-09-05 08:15:34 +0000

    update beanstalkd

--- util/docker/web/setup/beanstalkd.sh
+++ util/docker/web/setup/beanstalkd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -e
-source /bd_build/buildconfig
-set -x
+# set -e
+# source /bd_build/buildconfig
+# set -x
 
-apt-get update
+# apt-get update
 
 #$minimal_apt_get_install --allow-downgrades --allow-remove-essential --allow-change-held-packages --fix-broken beanstalkd
--- util/docker/web/startup_scripts/15_initialize.sh
+++ util/docker/web/startup_scripts/15_initialize.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt update && sudo apt install beanstalkd
+
 set -ex
 
 setuser azuracast azuracast_cli azuracast:setup:initialize
